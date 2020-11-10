55c7b11a4186ead370b3f0b2aa5f4cd4c80187ee
KaSliK <kamil.kaslikowski@gmail.com>
2020-11-10 13:13:00 +0100

    Make code more elegant, added adaptivebutton widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import '../widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -55,14 +56,14 @@ class _NewTransactionState extends State<NewTransaction> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-          child: Card(
+      child: Card(
         elevation: 5,
         child: Container(
           padding: EdgeInsets.only(
             top: 10,
             left: 10,
             right: 10,
-            bottom: MediaQuery.of(context).viewInsets.bottom +15,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 15,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
@@ -89,22 +90,10 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat('dd.MM.y').format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                    ? CupertinoButton(textColor: Theme.of(context).primaryColor,
-                      child: Text(
-                        'Choose date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: _presentDatePicker,
+                    AdaptiveFlatButton(
+                      'Choose Date',
+                      _presentDatePicker,
                     )
-                    : FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      child: Text(
-                        'Choose date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: _presentDatePicker,
-                    ),
                   ],
                 ),
               ),
